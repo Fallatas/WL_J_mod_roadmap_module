e891cacaeee9303ccba8d9374491b3fa2b9ebb18
Thomas Winterling <info@winterling-labs.com>
2019-11-12 13:56:07 +0100

    New joomla module

---
+++ helper.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @package    WL ROADMAP MODUL
+ *
+ * @author     Thomas Winterling <info@winterling-labs.com>
+ * @copyright  Copyright (C) 2005 - 2019. All rights reserved.
+ * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ **/
+
+defined('_JEXEC') or die;
+
+/**
+ * Helper for mod_roadmap_module
+ *
+ * @since  Version 1.0.0.0
+ */
+class ModWL_ROADMAP_Module_Helper
+{   
+    
+    
+
+}
---
+++ tmpl/default.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package    WL_ROADMAP_MODULE
+ *
+ * @author     Thomas Winterling <info@winterling-labs.com>
+ * @copyright  Copyright (C) 2011 - 2019
+ * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ **/
+
+defined('_JEXEC') or die;
+
+JHtml::_('stylesheet', 'mod_wl_roadmap_module/style.css', array('version' => 'auto', 'relative' => true));
+JHtml::_('script', 'mod_wl_roadmap_module/scripts.js', array('version' => 'auto', 'relative' => true));
+JHtml::_('jQuery.Framework');
+?>
